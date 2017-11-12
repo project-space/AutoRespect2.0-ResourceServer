2297437deba6aea2accfc9cff4a8deb7a5fe6f03
alex troshkin <alextroshkin@outlook.com>
2017-11-13 01:24:17 +0300

    Initial commit

---
+++ src/AutoRespect.ResourceServer.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AutoRespect.ResourceServer.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/AutoRespect.ResourceServer.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AutoRespect.ResourceServer.DataAccess
+{
+    public class Class1
+    {
+    }
+}
